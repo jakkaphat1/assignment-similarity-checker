88f389e7946a0823a0c80c897efc3f14395f99f3
jakkaphat1 <jakkaphat.w@kkumail.com>
2025-09-28 22:05:45 +0700

    Edit auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -4,47 +4,57 @@ from typing import Optional
 from supabase_client import supabase
 
 
-#สร้าง router สำหรับ authentication
+# สร้าง router สำหรับ authentication
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
 )
 
+
 async def get_current_user(authorization: Optional[str] = Header(None)):
     """ตรวจสอบ JWT จาก Header และดึงข้อมูลจาก Supabase"""
-    
+
     if authorization is None or not authorization.startswith("Bearer "):
         raise HTTPException(status_code=401, detail="Unauthorized")
-   
-    token = authorization.split(" ")[1]  #เอา Bearer token ออก
+
+    token = authorization.split(" ")[1]  # เอา Bearer token ออก
     try:
         user_response = supabase.auth.get_user(token)
         return user_response.user
     except Exception as e:
         raise HTTPException(status_code=401, detail="Invalid token")
-    
+
+
 @router.post("/signup")
-async def signup(email: str = Form(...), password: str = Form(...)): #(...) คือ การหาข้อมูลที่มาจาก Form data
-    """เป็น Endpoint สำหรับการสมัครสมาชิก"""
+async def signup(user_data: UserCreate):
+    """เป็น Endpoint สำหรับการสมัครสมาชิก รับข้อมูลแบบ JSON"""
     try:
-        res = supabase.auth.sign_up({"email": email, "password": password})
+        res = supabase.auth.sign_up({
+            "email": user_data.email,
+            "password": user_data.password,
+        })
         return {
-            "message" : "สมัครสมาชิกสำเร็จ",
+            "message": "สมัครสมาชิกสำเร็จ", 
             "user_id": res.user.id,
             "email": res.user.email
         }
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
+
+
 @router.post("/login")
-async def login(email: str = Form(...), password: str = Form(...)):
+async def login(user_data: UserLogin):
     """Endpoint สำหรับการเข้าสู่ระบบ"""
     try:
-        res = supabase.auth.sign_in_with_password({"email": email, "password": password})
+        res = supabase.auth.sign_in_with_password({
+            "email": user_data.email,
+            "password": user_data.password
+        })
         return {
-            "message" : "เข้าสู่ระบบสำเร็จ",
+            "message": "ล็อคอินสำเร็จ",
             "access_token": res.session.access_token,
             "token_type": "bearer"
         }
     except Exception as e:
-        raise HTTPException(status_code=401, detail="Invalid login credentials")
+        raise HTTPException(
+            status_code=401, detail="Invalid login credentials")
