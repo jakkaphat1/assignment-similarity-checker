56c399b4f95e57802728343e096390db8e6e110a
jakkaphat1 <jakkaphat.w@kkumail.com>
2025-10-06 11:42:18 +0700

    Edit Embedding.py cache and add black text on form in /signup

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -61,6 +61,7 @@ export default function SignupPage(){
             onChange={(e) => setFullName(e.target.value)}
             required
             className={styles.input}
+            
           />
         </div>
 
