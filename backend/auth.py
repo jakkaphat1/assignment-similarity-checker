9b1c0159b4b5a6d9126e306e4b9bdf2078cf4a2a
jakkaphat1 <jakkaphat.w@kkumail.com>
2025-10-07 10:29:31 +0700

    Edit text in auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -36,9 +36,9 @@ async def get_current_user(authorization: Optional[str] = Header(None)):
         user = user_response.user
 
         # Debug
-        print(f" User authenticated: {user.id}")
-        print(f"Header Token: {token[:30]}...")
-        print(f"Last Token: {token[-30:]}...")
+        print(f"User authenticated: {user.id}")
+        print(f"Header Token: {token[:30]}")
+        print(f"Last Token: {token[-30:]}")
 
         return user
     except Exception as e:
